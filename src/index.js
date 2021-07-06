f4441cccec155934005e08699fcfdc42bd389f88
Andrés K. Aquin <andruuus123@gmail.com>
2021-07-06 18:52:38 -0400

    Users Accounts and Authentication

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const { Token } = require('graphql');
 
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
@@ -22,11 +23,30 @@ const app = express();
 // Connect to the database
 db.connect(DB_HOST);
 
+// get the user info from a JWT
+const getUser = token => {
+  if(token) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      // if there's a problem with the token, throw an error
+      throw new Error('Session invalid');
+    }
+  }
+};
+
 // Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // for now, lets' log the user to the console
+    console.log(user);
     //Add the db models to the context
     return { models };
   }
