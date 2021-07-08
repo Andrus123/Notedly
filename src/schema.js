e9e76717b861f673b15aaae754e859b84dbc4b1e
Andrés K. Aquin <andruuus123@gmail.com>
2021-07-08 02:48:55 -0400

    Pagination

--- src/schema.js
+++ src/schema.js
@@ -12,6 +12,11 @@ module.exports = gql`
     favoriteCount: Int!
     favoritedBy: [User!]
   }
+  type NoteFeed {
+    notes: [Note]!
+    cursor: String!
+    hasNextPage: Boolean!
+  }
 
   type User {
     id: ID!
@@ -27,6 +32,7 @@ module.exports = gql`
     user(username: String!): User
     users: [User!]!
     me: User!
+    noteFeed(cursor: String): NoteFeed
   }
   type Mutation {
     newNote(content: String!): Note!
